c53f3a364a5e4df2bb633c632332e0e5b85914d6
nabeel ahmed <nabeelahmed377@gmail.com>
2024-10-27 03:32:38 +0500

    hello

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) =>
-  res.send("Express on Vercellllllllllllllllllllllll")
-);
+app.get("/", (req, res) => res.send("Express Budy"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
