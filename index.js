1e1675aa06b4f953123d025f53e2e581d46e682a
nabeel ahmed <nabeelahmed377@gmail.com>
2024-10-26 17:01:30 +0500

    new chame

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-// Import express
-import express from "express.js";
+const express = require("express");
 const app = express();
 
-// Define your routes
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
-app.get("/about", (req, res) => {
-  res.send("About Page");
-});
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// Catch-all route for undefined routes (404)
-app.use((req, res) => {
-  res.status(404).send("404 - Page Not Found");
-});
-
-// Start the server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
