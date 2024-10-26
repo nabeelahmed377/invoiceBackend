c04c802a42de76a46bdb6fd2bf6972dd49840d2f
nabeel ahmed <nabeelahmed377@gmail.com>
2024-10-27 03:37:37 +0500

    change

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 import express from "express";
 const app = express();
+import invoice from "./model/invoiceModel.js";
 
-app.get("/", (req, res) => res.send("Express Budy"));
+app.get("/", async (req, res) => {
+  try {
+    const Invoice = await invoice.find();
+    res.status(200).json({
+      status: true,
+      message: "All Invoice Found",
+      data: Invoice,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "Internal Server Error",
+      error,
+    });
+  }
+});
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3000, () => {
+  connectDB();
+  console.log("Server ready on port 3000.");
+});
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Database failed to connect: ", error);
+  }
+};
