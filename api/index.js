387a866028ca02214b3365e760a8e8dd279b43b4
nabeel ahmed <nabeelahmed377@gmail.com>
2024-10-27 16:45:46 +0500

    change in cores

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const app = express();
 // Enable CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://aagraphicsinvoice.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
